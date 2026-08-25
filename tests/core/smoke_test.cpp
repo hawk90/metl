ac4024b4b9f7fa66df0e88120112917cc3315787
hawk90 <hawking90a@gmail.com>
2026-08-25 10:30:24 +0900

    DO NOT MERGE: break the build to measure whether ci-gate blocks a merge

--- tests/core/smoke_test.cpp
+++ tests/core/smoke_test.cpp
@@ -4,3 +4,5 @@ int main() {
   static_assert(metl::version::major == 0, "unexpected major version");
   return 0;
 }
+
+static_assert(false, "deliberate break to measure ci-gate; this branch is thrown away");
